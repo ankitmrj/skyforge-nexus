edb79b4be46f456d5ce8abc45c34dc42adbeb99a
Ayushi Verma <ayushiverma6389@gmail.com>
2025-08-26 00:33:16 +0530

    finally :)

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -181,7 +181,7 @@ const Projects = () => {
               Our Projects
             </h1>
             <p className="text-xl text-muted-foreground leading-relaxed">
-             "Explore our visionary Drone and IoT innovations — redefining what’s possible by shaping the future of technology and inspiring smarter, sustainable, and impactful solutions for a safer, more connected world."
+             Explore our visionary Drone and IoT innovations — redefining what’s possible by shaping the future of technology and inspiring smarter, sustainable, and impactful solutions for a safer, more connected world.
             </p>
           </motion.div>
         </div>
